6e4e1eddbd193ba68ce8f0d7c5bb8eafb9340ecc
EmmanuelCorn <emmanuel.corn@>
2022-05-02 16:41:24 +0200

    HeidiSQL

--- CommandesProject/Program.cs
+++ CommandesProject/Program.cs
@@ -18,7 +18,6 @@ namespace CommandesProject
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            Console.Write("test");
         }
     }
 }
