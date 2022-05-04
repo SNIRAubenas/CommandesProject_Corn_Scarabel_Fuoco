e9ec82959401b8eb8c87e8ded5a0a4a9700af160
vincent_scarabel <vincent.scarabel@>
2022-05-04 11:55:53 +0200

    a finir vincent

--- CommandesProject/Form1.cs
+++ CommandesProject/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +14,44 @@ namespace CommandesProject
 {
     public partial class Form1 : System.Windows.Forms.Form
     {
+        SQLiteConnection sqlite_conn;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
 
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            var ligne = this.dataGridView1.Rows[e.RowIndex];
-            var element = ligne.Cells["id"];
-            //
+            if (!File.Exists("CommandesProject.db"))
+            {
+                MessageBox.Show("Le fichier CommandesProject.db est absent","Erreur",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            sqlite_conn = new SQLiteConnection("Data Source=CommandesProject.db ");
+
             try
             {
-                
-
-
+                sqlite_conn.Open();
 
             }
-            catch { }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erreur à l'ouverture " + Environment.NewLine + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            //
+            SQLiteCommand cmd;
+            cmd = new SQLiteCommand("SELECT id , nom , prenom FROM adresses" , sqlite_conn);
+            //
+            DataTable extrait = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(extrait);
+            this.
+
         }
     }
 }
