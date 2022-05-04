7b0688d8b6e447399707051cbdc592d4ec5610f6
vincent_scarabel <vincent.scarabel@>
2022-05-04 11:04:39 +0200

    test 1

--- CommandesProject/Form1.cs
+++ CommandesProject/Form1.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace CommandesProject
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var ligne = this.dataGridView1.Rows[e.RowIndex];
+            var element = ligne.Cells["id"];
+            //
+            try
+            {
+                
+
+
+
+            }
+            catch { }
         }
     }
 }
